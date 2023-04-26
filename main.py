56f49f919fccdda515f1c692a31575e829728d1f
Tikita Peralta <tikitaperalta@gmail.com>
2023-04-26 17:35:29 +0700

    added for loop

--- main.py
+++ main.py
@@ -1,56 +1,65 @@
-#!/usr/bin/env python3
 import pygame, sys, math
 
-# Here's our constants, constants (i.e. things that shouldn't change) are typically written in caps 
-SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 480, 480
+
+SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 900, 900
 SCREEN_CENTER = SCREEN_WIDTH_CENTER, SCREEN_HEIGHT_CENTER = SCREEN_WIDTH/2, SCREEN_HEIGHT/2
-BLACK = (0,0,0) # These are colours defined in RGB codes
+BLACK = (0,0,0)
 WHITE = (255,255,255) 
-ORBIT_RADIUS = 100
+
+def store_planet_info():
+    planet_store = {1 : {'planet_name': 'mercury', 'radius': 5, 'orbit_radius': 100, 'color': (150,75,0)},
+                    2 : {'planet_name': 'venus', 'radius': 20, 'orbit_radius': 150, 'color': (124,252,0)},
+                    3 : {'planet_name': 'earth', 'radius': 20, 'orbit_radius': 200, 'color': (0,0,255)},
+                    4 : {'planet_name': 'mars', 'radius': 15, 'orbit_radius': 250, 'color': (255,0,0)},
+                    5 : {'planet_name': 'jupiter', 'radius': 100, 'orbit_radius': 400, 'color': (255,165,0)},
+                    6 : {'planet_name': 'saturn', 'radius': 50, 'orbit_radius': 500, 'color': (255,255,0)},
+                    7 : {'planet_name': 'uranus', 'radius': 40, 'orbit_radius': 600, 'color': (0,191.255)},
+                    8 : {'planet_name': 'neptune', 'radius': 30, 'orbit_radius': 700, 'color': (221,160,221)}}
+    return planet_store
+    '''
+    
 
 def compute_planet_center(current_time):
-
-    # We want one rotation every second
-    #current_time_milliseconds = current_time % 1000 # This gets us the number of milliseconds after every second, note that the unit of current time is milliseconds
-    current_time_milliseconds_as_fraction = current_time/1000.0 # Gotta use a float to get a float
-    
-    PLANET_X = SCREEN_WIDTH_CENTER + (math.sin(current_time_milliseconds_as_fraction) * ORBIT_RADIUS)
-    PLANET_Y = SCREEN_HEIGHT_CENTER + (math.cos(current_time_milliseconds_as_fraction) * ORBIT_RADIUS)
-
+    current_time_milliseconds_as_fraction = current_time/1000.0
+    PLANET_X = SCREEN_WIDTH_CENTER + (math.sin(current_time_milliseconds_as_fraction) * orbit_radius)
+    PLANET_Y = SCREEN_HEIGHT_CENTER + (math.cos(current_time_milliseconds_as_fraction) * orbit_radius)
     return PLANET_X, PLANET_Y
+'''
 
 def main():
-    """
-    This is the main function for our game
-
-    """
-    pygame.init() # Initialise pygame, always required
-    screen = pygame.display.set_mode(SCREEN_SIZE) # Screen is the 'canvas' we will paint our game onto 
-
-    running = True # Variable we use to track if the game is running
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    running = True
     Clock = pygame.time.Clock()
 
     while running:
-
-        # Get the amount of time that has passed since last loop
         Clock.tick(60)
         current_time = pygame.time.get_ticks()
 
-        # Stop the game if the user clicks the quit button in the corner
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                  sys.exit()
 
-        screen.fill(BLACK) # Paint the entire screen black
-        pygame.draw.circle(screen, WHITE, SCREEN_CENTER, 15) # Paint a white circle in the middle of the screen.
+        screen.fill(BLACK)
+        sun = pygame.draw.circle(screen, WHITE, SCREEN_CENTER, 15)
 
-        planet_center = compute_planet_center(current_time)
-        print(current_time)
+        #planet_center = compute_planet_center(current_time)
+        #print(current_time)
 
-        pygame.draw.circle(screen, WHITE, planet_center,15)
+        planet_info = store_planet_info()
+        for planet_id, planet in planet_info.items():
+            planet_name = planet['planet_name']
+            orbit_radius = planet['orbit_radius']
+            compute_planet_center()
+            
+            
+                
 
-        pygame.display.flip() # Flip the screen to show it to the user!
 
-""" This functions means that if we execute this file using ./main.py, run the main function"""
+        #pygame.draw.circle(screen, WHITE, planet_center, 15)
+
+        pygame.display.flip()
+
+
 if __name__ == "__main__":
     main()
